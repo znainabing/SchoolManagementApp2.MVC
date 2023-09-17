7f234d56e6770546ce5c4f435d4b439f720e576e
Zainab HS <hszainab2@gmail.com>
2023-09-17 16:15:27 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolManagementApp2.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
